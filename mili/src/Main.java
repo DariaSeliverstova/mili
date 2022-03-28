8140e643a43ab57608abd082e1b271159725e538
DariaSeliverstova <dashulia2004@yandex.ru>
2022-03-28 15:15:57 -0400

    added files

---
+++ mili/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 500;
+        int miliPrice = 20;
+        int mili = ticketPrice / miliPrice;
+        System.out.println("количество миль составляет :" + mili);
+    }
+}
